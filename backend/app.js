8cee8eca5018295c8122352a0b272ed3c203384e
mchrys-dev <mchrys-dev@gmail;com>
2022-06-21 17:16:11 +0200

    Création du backend

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://marchrys:Mar051731@piiquante.lhf1s.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log(error));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+module.exports = app;
